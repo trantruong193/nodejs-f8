d062254c4b7310b06a2ccb58198db55235e74196
trantruong193 <trantruong193@gmail.com>
2023-02-01 15:01:29 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const app = express()
+const port = 3000;
+
+app.get('/',(req,res) => res.send('Hello'))
+
+app.listen(port,() => console.log(`app listen on port: ${port}`))
